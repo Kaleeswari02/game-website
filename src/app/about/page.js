0f1ec67ba39bb01dada23ea98de5544dbb9e68ef
Kaleeswari02 <kaleeskaleeswari02@gmail.com>
2025-05-26 14:37:41 +0530

    changes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,14 +1,38 @@
-// components/About.js
+"use client"
 import Image from 'next/image';
 import girlImg from './../../../public/images/char-1.png'; 
 import tigerImg from './../../../public/images/char-2.png'; 
 import styles from './about.module.css';
-import { FiArrowUpRight } from 'react-icons/fi'; // import arrow icon
-
+import { FiArrowUpRight ,FiArrowRight} from 'react-icons/fi'; 
+import { useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+import CapabilitiesSection from '../capabilities/page';
+gsap.registerPlugin(ScrollTrigger);
 
 export default function About() {
+    const tigerRef = useRef(null);
+    const sectionRef = useRef(null);
+      useGSAP(() => {
+    gsap.fromTo(
+      tigerRef.current,
+      { x: '-150px' },
+      {
+        x: '150px',
+        ease: 'none',
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: 'top bottom',
+          end: 'bottom top',
+          scrub: true,
+        },
+      }
+    );
+  }, { scope: sectionRef }); 
   return (
-      <div className={`container-fluid  mt-5 mt-xl-5 pt-5 ${styles.aboutSection}`}>
+    <>
+      <div ref={sectionRef} className={`container-fluid  mt-5 mt-xl-5 pt-5 ${styles.aboutSection}`}>
       <div className={`container`}>
 
         <div className="row align-items-center">
@@ -18,7 +42,7 @@ export default function About() {
 
           <div className="col-md-6 text-center text-md-start">
             
-            <h2 className={`${styles.aboutHeading}`}>
+            <h2 className={`${styles.aboutHeading} text-center`}>
               Casino Slot Game Development<br/> Company
             </h2>
             <div className='p-3'>
@@ -26,22 +50,30 @@ export default function About() {
              Our studio is a hub of creativity where every pixel tells a story. From concept art to character design, our talented artists breathe life into every aspect of our games. We believe in pushing the boundaries of visual storytelling, creating captivating environments that draw players into rich, immersive worlds.
             </p>
             <p className={`${styles.aboutPara}`}>
-              As a leading <strong className='text-black'>Casino Slot Game Development Company in the USA</strong>, our team of skilled engineers and programmers harness the latest tools and technologies to build seamless gaming experiences. Whether it’s crafting complex gameplay mechanics or optimizing performance across platforms, we are committed to delivering games that captivate and inspire.
+              As a leading <a href="#">Casino Slot Game Development Company in the USA</a>, our team of skilled engineers and programmers harness the latest tools and technologies to build seamless gaming experiences. Whether it’s crafting complex gameplay mechanics or optimizing performance across platforms, we are committed to delivering games that captivate and inspire.
             </p>
-            <div className='text-center'>
-            <button className= {`${styles.aboutButton}  px-4 mt-3`}>
-              GET ASSISTANCE <span className={`${styles.aboutButtonArrow}`}><FiArrowUpRight /></span>
+                    <div className="text-center">
+            <button className={`${styles.aboutButton} px-4 mt-3`}>
+                GET ASSISTANCE
+                <span className={`${styles.aboutButtonArrow} ms-2`}>
+                <FiArrowUpRight className={styles.arrowDefault} />
+                <FiArrowRight className={styles.arrowHover} />
+                </span>
             </button>
             </div>
+
             </div>
-          
           </div>
 
           <div className="col-md-3 text-center mt-4 mt-md-0">
-            <Image src={tigerImg} alt="Tiger Character" className={styles.characterImg} />
+            <div ref={tigerRef}>
+              <Image src={tigerImg} alt="Tiger Character" className={styles.characterImg} />
+            </div>
           </div>
         </div>
         </div>
       </div>
+      <CapabilitiesSection/>
+      </>
   );
 }
---
+++ src/app/capabilities/page.js
@@ -0,0 +1,142 @@
+import { useState } from 'react';
+import {
+  Nav,
+  NavItem,
+  NavLink,
+  TabContent,
+  TabPane,
+  Row,
+  Col
+} from 'reactstrap';
+import classnames from 'classnames';
+import Image from 'next/image';
+import { FiArrowUpRight ,FiArrowRight} from 'react-icons/fi';
+import Capa1 from './../../../public/images/capa-1.png'; 
+import Capa2 from './../../../public/images/capa-2.png'; 
+import Capa3 from './../../../public/images/capa-3.png'; 
+import styles from './capabilities.module.css';
+import stylesAbout from '../about/about.module.css'
+const CapabilitiesSection = () => {
+  const [activeTab, setActiveTab] = useState('1');
+
+  return (
+    <div className={`${styles.capaSection} container-fluid  py-5`}>
+      <h2 className="text-center">
+        <span className={`${styles.capaHeading}`}>OUR</span> 
+        <span className={`${styles.textOutline}`}>CAPABILITIES</span>
+      </h2>
+
+      <Nav tabs className="justify-content-center mt-4 border-bottom">
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '1' })}
+            onClick={() => setActiveTab('1')}
+          >
+            GAME ARTWORK
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '2' })}
+            onClick={() => setActiveTab('2')}
+          >
+            GAME DEVELOPMENT
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '3' })}
+            onClick={() => setActiveTab('3')}
+          >
+            MULTIPLATFORM DEVELOPMENT
+          </NavLink>
+        </NavItem>
+      </Nav>
+
+      <TabContent activeTab={activeTab} className="mt-4">
+       <TabPane tabId="1" fade={false}>
+        <Row className="align-items-center">
+            <Col md={6} className="d-flex justify-content-center align-items-center">
+            <Image
+                src={Capa1}
+                alt="Queen Character"
+                className={styles.characterImage}
+            />
+            </Col>
+            <Col md={6}>
+            <h3 className={`${stylesAbout.aboutHeading}`}>Game Artwork</h3>
+            <p className={`${stylesAbout.aboutPara} mt-3`}>
+                Gamecrio Studios, a premier <a href="#" className={`${stylesAbout.aboutPara}`}>
+                    Casino Slot Game Artwork Company in the USA</a>, believes game artwork is more than just pixels on a 
+                    screen—it’s a canvas for creativity, imagination, and storytelling. From concept art to final renders, 
+                    every brushstroke and pixel is carefully crafted to evoke emotion,
+                     immerse players in new worlds, and bring our games to life.
+            </p>
+        
+              <button className={`${stylesAbout.aboutButton} px-4 mt-3`}>
+               ARTWORK SUPPORT
+                <span className={`${stylesAbout.aboutButtonArrow} ms-2`}>
+                <FiArrowUpRight className={stylesAbout.arrowDefault} />
+                <FiArrowRight className={stylesAbout.arrowHover} />
+                </span>
+            </button>
+            </Col>
+        </Row>
+        </TabPane>
+
+        <TabPane tabId="2" fade={false}>
+        <Row className="align-items-center">
+            <Col md={6} className="d-flex justify-content-center align-items-center">
+            <Image
+                src={Capa2}
+                alt="Queen Character"
+                className={styles.characterImage}
+            />
+            </Col>
+            <Col md={6}>
+            <h3 className={`${stylesAbout.aboutHeading}`}>Game Artwork</h3>
+            <p className={`${stylesAbout.aboutPara} mt-3`}>
+               At Gamecrio, a leading  <a href="#" className="text-primary fw-bold"> Casino Slot Game Development Company in USA </a>, 
+                    designing and developing games is a team effort driven by creativity, enthusiasm, and knowledge. We carefully plan, create, and optimize every element of our games—from AI systems to gameplay mechanics—to provide fluid, amazing gameplay that appeals players to keep playing.
+            </p>
+            <button className={`${stylesAbout.aboutButton} px-4 mt-3`}>
+               ARTWORK SUPPORT
+                <span className={`${stylesAbout.aboutButtonArrow} ms-2`}>
+                <FiArrowUpRight className={stylesAbout.arrowDefault} />
+                <FiArrowRight className={stylesAbout.arrowHover} />
+                </span>
+            </button>
+            </Col>
+        </Row>
+        </TabPane>
+
+        <TabPane tabId="3" fade={false}>
+        <Row className="align-items-center">
+            <Col md={6} className="d-flex justify-content-center align-items-center">
+            <Image
+                src={Capa3}
+                alt="Queen Character"
+                className={styles.characterImage}
+            />
+            </Col>
+            <Col md={6}>
+            <h3 className={`${stylesAbout.aboutHeading}`}>Game Artwork</h3>
+            <p className={`${stylesAbout.aboutPara} mt-3`}>
+                Gamecrio Studios, a premier <a href="#" className="text-primary fw-bold">Casino Slot Game Artwork Company in the USA</a>we develop games for various platforms, including PC, console, mobile, and virtual reality. Our proficiency with several platforms enables us to optimize outreach and interaction while upholding a uniform and superior user experience across various devices.
+            </p>
+             <button className={`${stylesAbout.aboutButton} px-4 mt-3`}>
+               ARTWORK SUPPORT
+                <span className={`${stylesAbout.aboutButtonArrow} ms-2`}>
+                <FiArrowUpRight className={stylesAbout.arrowDefault} />
+                <FiArrowRight className={stylesAbout.arrowHover} />
+                </span>
+            </button>
+            </Col>
+        </Row>
+        </TabPane>
+      </TabContent>
+    </div>
+  );
+};
+
+export default CapabilitiesSection;
