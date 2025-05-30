53fc9f306e0d7a058db288cfcbf745853a2363cc
Kaleeswari02 <kaleeskaleeswari02@gmail.com>
2025-05-30 11:56:04 +0530

    mobile-responsive worked

--- src/app/header/page.js
+++ src/app/header/page.js
@@ -9,8 +9,26 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { toast } from 'react-toastify';
 import { useModal } from '../context/ModalContext';
+import { CgMenuRight } from "react-icons/cg";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Header() {
+  const scrollToAndClose = (e, id) => {
+    e.preventDefault();
+  
+    // Trigger a click on the close button
+    const closeBtn = document.querySelector('#offcanvasRight .btn-close');
+    if (closeBtn) closeBtn.click();
+  
+    // Scroll after a slight delay (to let canvas close)
+    setTimeout(() => {
+      const target = document.querySelector(id);
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 300);
+  };
+  
   const formRef = useRef();
 
   // const [modal, setModal] = useState(false);
@@ -57,93 +75,217 @@ export default function Header() {
 
   return (
     <>
-      <div className={`container-fluid header-fluid fixed-top transition-all`} >
-        <div className="header-area p-xl-1 p-lg-1 p-md-2">
-          <div className="row">
-            <div className="col-xl-2 col-lg-2 col-md-2">
-              <div className='logo-image p-xl-1'>
-                <Link href="/">
-                  <img className='logo-image-bg' src='/images/logo-white.svg' alt="Description of image" width={150} height={60} />
-                </Link></div>
-            </div>
-            <div className="col-xl-8 col-lg-8 col-md-8">
-              <nav className="navbar-area mt-xl-4">
-                <ul className="nabar-section">
-                  <li><Link className="nav-item" href="/">Home</Link></li>
-                  <li><Link className="nav-item" href="#about">About us</Link></li>
-                  <li><Link className="nav-item" href="#Services">Services</Link></li>
-                  <li><Link className="nav-item" href="#portfolio">Portfolio</Link></li>
-                  <li><Link className="nav-item" href="#testimonal">Testimonials</Link></li>
-                  <li><Link className="nav-item" href="/careers">Careers</Link></li>
-                </ul>
-              </nav>
-            </div>
-            <div className="col-xl-2 col-lg-2 col-md-2 mt-xl-2">
-              <div className='button-space'>
-                <button onClick={openModal}
-                  className='contact-btn'>
-                  Contact Us
-                </button>
+    <div className='desktop-view'>
+      <div className={`container-fluid header-fluid fixed-top transition-all desktop-view`} >
+          <div className="header-area p-xl-1 p-lg-1 p-md-2">
+            <div className="row">
+              <div className="col-xl-2 col-lg-2 col-md-2">
+                <div className='logo-image p-xl-1'>
+                  <Link href="/">
+                    <img className='logo-image-bg' src='/images/logo-white.svg' alt="Description of image" width={150} height={60} />
+                  </Link></div>
+              </div>
+              <div className="col-xl-8 col-lg-8 col-md-8">
+                <nav className="navbar-area mt-xl-4">
+                  <ul className="nabar-section">
+                    <li><Link className="nav-item" href="/">Home</Link></li>
+                    <li><Link className="nav-item" href="#about">About us</Link></li>
+                    <li><Link className="nav-item" href="#Services">Services</Link></li>
+                    <li><Link className="nav-item" href="#portfolio">Portfolio</Link></li>
+                    <li><Link className="nav-item" href="#testimonal">Testimonials</Link></li>
+                    <li><Link className="nav-item" href="/careers">Careers</Link></li>
+                  </ul>
+                </nav>
+              </div>
+              <div className="col-xl-2 col-lg-2 col-md-2 mt-xl-2">
+                <div className='button-space'>
+                  <button onClick={openModal}
+                    className='contact-btn'>
+                    Contact Us
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* Contact Modal */}
-      {/* <Modal isOpen={modal} toggle={toggle} centered backdrop="static" >
-        <ModalHeader toggle={toggle}>Contact Us</ModalHeader> */}
-     <Modal isOpen={isModalOpen} toggle={closeModal} centered backdrop="static">
-  <ModalHeader toggle={closeModal}>Contact Us</ModalHeader>
-        <form ref={formRef} onSubmit={handleSubmit(sendEmail)}>
-          <ModalBody>
-            <FormGroup>
-              <Label for="name">Name</Label>
-              <input
-                id="name"
-                type="text"
-                autoComplete="off"
-                {...register("name")}
-                className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+        {/* Contact Modal */}
+        {/* <Modal isOpen={modal} toggle={toggle} centered backdrop="static" >
+          <ModalHeader toggle={toggle}>Contact Us</ModalHeader> */}
+      <Modal isOpen={isModalOpen} toggle={closeModal} centered backdrop="static">
+    <ModalHeader toggle={closeModal}>Contact Us</ModalHeader>
+          <form ref={formRef} onSubmit={handleSubmit(sendEmail)}>
+            <ModalBody>
+              <FormGroup>
+                <Label for="name">Name</Label>
+                <input
+                  id="name"
+                  type="text"
+                  autoComplete="off"
+                  {...register("name")}
+                  className={`form-control ${errors.name ? 'is-invalid' : ''}`}
 
-              />
-              <div className="invalid-feedback">{errors.name?.message}</div>
-            </FormGroup>
+                />
+                <div className="invalid-feedback">{errors.name?.message}</div>
+              </FormGroup>
 
-            <FormGroup>
-              <Label for="email">Email</Label>
-              <input
-                id="email"
-                type="email"
-                autoComplete="off"
-                {...register("email")}
-                className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-              />
-              <div className="invalid-feedback">{errors.email?.message}</div></FormGroup>
+              <FormGroup>
+                <Label for="email">Email</Label>
+                <input
+                  id="email"
+                  type="email"
+                  autoComplete="off"
+                  {...register("email")}
+                  className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                />
+                <div className="invalid-feedback">{errors.email?.message}</div></FormGroup>
 
-            <FormGroup>
-              <Label for="message">Message</Label>
-              <textarea
-                id="message"
-                type="textarea"
-                autoComplete="off"
-                rows={4}
-                {...register("message")}
-                className={`form-control ${errors.message ? 'is-invalid' : ''}`}
-              />
-              <div className="invalid-feedback">{errors.message?.message}</div>
-            </FormGroup>
-          </ModalBody>
+              <FormGroup>
+                <Label for="message">Message</Label>
+                <textarea
+                  id="message"
+                  type="textarea"
+                  autoComplete="off"
+                  rows={4}
+                  {...register("message")}
+                  className={`form-control ${errors.message ? 'is-invalid' : ''}`}
+                />
+                <div className="invalid-feedback">{errors.message?.message}</div>
+              </FormGroup>
+            </ModalBody>
 
-          <ModalFooter>
-            <Button className='my-3' type="submit" color="primary">
-              Submit
-            </Button>
-            {/* <Button color="secondary" onClick={toggle}>
-              Cancel
-            </Button> */}
-          </ModalFooter>
-        </form>
+            <ModalFooter>
+              <Button className='my-3' type="submit" color="primary">
+                Submit
+              </Button>
+              {/* <Button color="secondary" onClick={toggle}>
+                Cancel
+              </Button> */}
+            </ModalFooter>
+          </form>
       </Modal>
+    </div>
+    <div className="mobile-view">
+      <div className="container fixed-top mobile-view-bg">
+        <div className="row">
+          {/* Logo */}
+          <div className="col-6">
+            <div className="logo-sec m-2">
+              <Link href="/">
+                <img
+                  className="logo-image-bg"
+                  src="/images/logo.png"
+                  alt="Logo"
+                  width={150}
+                  height={60}
+                />
+              </Link>
+            </div>
+          </div>
+
+          {/* Mobile Menu Button */}
+          <div className="col-6">
+            <div className="m-right">
+              <button
+                type="button"
+                data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasRight"
+                aria-controls="offcanvasRight"
+                className="mobile-view-button"
+              >
+                <CgMenuRight style={{ color: '#000000' }} />
+              </button>
+            </div>
+
+            {/* Offcanvas Menu */}
+            <div
+              className="offcanvas offcanvas-end background-design-mobile"
+              tabIndex="-1"
+              id="offcanvasRight"
+              aria-labelledby="offcanvasRightLabel"
+            >
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasRightLabel">Beeyoond Gaming</h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="offcanvas-body">
+                <nav className="navbar-mobile-area">
+                  <ul className="nabar-mobile-section">
+                    <li><a className="nav-item" href="/" >Home</a></li>
+                    <li><a className="nav-item" href="#about" onClick={(e) => scrollToAndClose(e, '#about')}>About us</a></li>
+                    <li><a className="nav-item" href="#Services" onClick={(e) => scrollToAndClose(e, '#Services')}>Services</a></li>
+                    <li><a className="nav-item" href="#portfolio" onClick={(e) => scrollToAndClose(e, '#portfolio')}>Portfolio</a></li>
+                    <li><a className="nav-item" href="#testimonal" onClick={(e) => scrollToAndClose(e, '#testimonal')}>Testimonials</a></li>
+                    <li><Link className="nav-item" href="/careers">Careers</Link></li>
+                  </ul>
+                </nav>
+                <button onClick={openModal} className='contact-btn'>Contact Us</button>
+                <Modal isOpen={isModalOpen} toggle={closeModal} centered backdrop="static">
+                    <ModalHeader toggle={closeModal}>Contact Us</ModalHeader>
+                    <form ref={formRef} onSubmit={handleSubmit(sendEmail)}>
+                      <ModalBody>
+                        <FormGroup>
+                          <Label for="name">Name</Label>
+                          <input
+                            id="name"
+                            type="text"
+                            autoComplete="off"
+                            {...register("name")}
+                            className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+
+                          />
+                          <div className="invalid-feedback">{errors.name?.message}</div>
+                        </FormGroup>
+
+                        <FormGroup>
+                          <Label for="email">Email</Label>
+                          <input
+                            id="email"
+                            type="email"
+                            autoComplete="off"
+                            {...register("email")}
+                            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                          />
+                          <div className="invalid-feedback">{errors.email?.message}</div></FormGroup>
+
+                        <FormGroup>
+                          <Label for="message">Message</Label>
+                          <textarea
+                            id="message"
+                            type="textarea"
+                            autoComplete="off"
+                            rows={4}
+                            {...register("message")}
+                            className={`form-control ${errors.message ? 'is-invalid' : ''}`}
+                          />
+                          <div className="invalid-feedback">{errors.message?.message}</div>
+                        </FormGroup>
+                      </ModalBody>
+
+                      <ModalFooter>
+                        <Button className='my-3' type="submit" color="primary">
+                          Submit
+                        </Button>
+                        {/* <Button color="secondary" onClick={toggle}>
+                          Cancel
+                        </Button> */}
+                      </ModalFooter>
+                    </form>
+                </Modal>
+              </div>
+            </div>   
+          </div>
+        </div>
+      </div>
+    </div>
+
     </>
+
+        
+
   );
 }
